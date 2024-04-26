8bf78dfc38827878313ea1f94ca94202b7eedad4
zfnjoshua <155145273+zfnjoshua@users.noreply.github.com>
2024-04-26 17:58:32 +0300

    improve

--- commands/fun/gay.js
+++ commands/fun/gay.js
@@ -1,25 +1,43 @@
-const db = require("quick.db")
+const db = require("discord.js-commando").QuickDB;
 const Discord = require('discord.js');
-const { checkperm, between } = require("../../base/functions");
+const { checkperm } = require("../../base/functions");
+
+const colorRegex = /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/;
+
 module.exports = {
     name: "gay",
-    description: "Calcul le pourcentage d'homosexualité du membre",
+    description: "Calculates the percentage of homosexuality of a member",
 
     run: async (client, message, args, cmd) => {
-        let perm = await checkperm(message,cmd.name)
-        if (perm == true) {
-            let member = await message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(r => r.user.tag.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.guild.members.cache.find(r => r.displayName.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.member
-            if (!member || member.user.bot) return message.channel.send(`:x: Utilisateur invalide !`)
+        let perm = await checkperm(message, cmd.name);
+        if (perm === false) return message.reply(`:x: You don't have permission to use the command \`${cmd.name}\`!`);
 
-            const randomnumber = between(1, 100)
-            const random = new Discord.MessageEmbed()
-                .setTitle(member.user.username + ` est gay à ${randomnumber}% :rainbow_flag:`)
-                .setColor(db.fetch(`${message.guild.id}.color`))
-                .setImage(`https://i.imgur.com/MRpzCqz.gif?noredirect`)
-                .setFooter({text:"⚠️ Le pourcentage peut changer selon les pulsions du membre"})
+        let member;
+        try {
+            member = await message.guild.members.fetch(args[0]).catch(() => {});
+        } catch (e) {
+            return message.channel.send(`:x: Invalid user!`);
+        }
 
-            message.reply({embeds: [random]})
-        } else if(perm === false) if(!db.fetch(`${message.guild.id}.vent`)) return message.reply(`:x: Vous n'avez pas la permission d'utiliser la commande \`${cmd.name}\` !`)
+        if (!member) {
+            member = message.mentions.members.first() || message.member;
+            if (!member) return message.channel.send(`:x: Invalid user!`);
+        }
 
+        if (member.user.bot) return message.channel.send(`:x: Bots don't have sexuality!`);
+
+        const randomNumber = Math.floor(Math.random() * 100) + 1;
+
+        if (!db.fetch(`${message.guild.id}.color`) || !colorRegex.test(db.fetch(`${message.guild.id}.color`))) {
+            db.set(`${message.guild.id}.color`, "#FF0000");
+        }
+
+        const embed = new Discord.MessageEmbed()
+            .setTitle(`${member.user.username} is gay at ${randomNumber}% :rainbow_flag:`)
+            .setColor(db.fetch(`${message.guild.id}.color`))
+            .setImage(`https://i.imgur.com/MRpzCqz.gif?noredirect`)
+            .setFooter({text:"⚠️ The percentage may change if the member is feeling frisky"});
+
+        message.reply({embeds: [embed]});
     }
 }
