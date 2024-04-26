7d7c024e77f2f21b2c0db3af8113a3b5c0d89640
zfnjoshua <155145273+zfnjoshua@users.noreply.github.com>
2024-04-26 17:58:34 +0300

    improve

--- commands/config/ghostping.js
+++ commands/config/ghostping.js
@@ -1,44 +1,57 @@
-const db = require("quick.db")
-const { MessageEmbed } = require("discord.js")
+const db = require("quick.db");
+const { MessageEmbed } = require("discord.js");
 const { checkperm } = require("../../base/functions");
+
 module.exports = {
-    name: "ghostping",
-    description: "Défini les salons de ghostping",
-    aliases: ['ghost-ping', 'gp'],
+  name: "ghostping",
+  description: "Define ghostping channels",
+  aliases: ["ghost-ping", "gp"],
 
-    run: async (client, message, args, cmd) => {
-        let perm = await checkperm(message,cmd.name)
-        if (perm == true) {
-            if (args[0] && args[0].toLowerCase() === "list") {
-                let gpchannels = db.fetch(`${message.guild.id}.ghostping`)
-                for (i in gpchannels) {
-                    let check = message.guild.channels.cache.get(gpchannels[i])
-                    if (!check) {
-                        const filtered = gpchannels.filter(e => e !== gpchannels[i]);
-                        db.set(`${message.guild.id}.ghostping`, filtered);
-                    }
-                }
-                message.reply({
-                    embeds: [new MessageEmbed()
-                        .setColor(db.fetch(`${message.guild.id}.color`))
-                        .setTitle(`Salons ghostping`)
-                        .setDescription(`${gpchannels && gpchannels.length > 0 ? gpchannels.map(r => message.guild.channels.cache.get(r) ? `<#${r}>` : "").join("\n") : "Aucun salons"}\n\n_Refaites la commande \`ghostping\` en mentionnant le salon pour désactiver_`)
-                        .setFooter({ text: `Les nouveaux membres seront ghostping dans ces salons` })]
-                })
-                return
-            }
-            let m = message.mentions.channels.first() || message.guild.channels.cache.get(args[1]) || message.channel
-            if (!m || m.type !== "GUILD_TEXT") return message.reply(`:x: Salon invalide !`)
-            let actual = db.fetch(`${message.guild.id}.ghostping`)
-            if (actual && actual.includes(m.id)) {
-                const filtered = actual.filter(e => e !== m.id);
-                db.set(`${message.guild.id}.ghostping`, filtered);
-                return message.reply(`:balloon: <#${m.id}> n'est plus un salon ghostping !`)
-            }
-            db.push(`${message.guild.id}.ghostping`, m.id)
-            return message.reply(`:balloon: <#${m.id}> est maintenant un salon ghostping !\n_Utilisez \`ghostping list\` pour voir tous les salons ghostping_`)
+  run: async (client, message, args, cmd) => {
+    const perm = await checkperm(message, cmd.name);
+    if (!perm) {
+      if (!db.fetch(`${message.guild.id}.vent`))
+        return message.reply(`:x: You don't have permission to use the command \`${cmd.name}\`!`);
+    } else if (perm === true) {
+      const listOption = args[0]?.toLowerCase() === "list";
+      if (listOption) {
+        const ghostPingChannels = db.fetch(`${message.guild.id}.ghostping`);
+        if (!ghostPingChannels || !ghostPingChannels.length) {
+          return message.reply("No ghostping channels found.");
+        }
 
+        const channelList = ghostPingChannels.map((id) => {
+          const channel = message.guild.channels.cache.get(id);
+          return channel ? `<#${channel.id}>` : "";
+        }).join("\n");
 
-        } else if(perm === false) if(!db.fetch(`${message.guild.id}.vent`)) return message.reply(`:x: Vous n'avez pas la permission d'utiliser la commande \`${cmd.name}\` !`)
+        return message.reply({
+          embeds: [
+            new MessageEmbed()
+              .setColor(db.fetch(`${message.guild.id}.color`))
+              .setTitle("Ghostping Channels")
+              .setDescription(channelList || "No ghostping channels found.\n\nUse \`ghostping\` with a channel mention to enable.")
+              .setFooter({ text: "New members will be ghostpinged in these channels." })
+          ]
+        });
+      }
+
+      const channel = message.mentions.channels.first() || message.guild.channels.cache.get(args[0]) || message.channel;
+      if (!channel || channel.type !== "GUILD_TEXT")
+        return message.reply(`:x: Invalid channel!`);
+
+      const ghostPingChannels = db.fetch(`${message.guild.id}.ghostping`) || [];
+
+      const index = ghostPingChannels.findIndex((id) => id === channel.id);
+      if (index > -1) {
+        ghostPingChannels.splice(index, 1);
+        db.set(`${message.guild.id}.ghostping`, ghostPingChannels);
+        return message.reply(`:balloon: <#${channel.id}> is no longer a ghostping channel!`);
+      }
+
+      ghostPingChannels.push(channel.id);
+      db.set(`${message.guild.id}.ghostping`, ghostPingChannels);
+      return message.reply(`:balloon: <#${channel.id}> is now a ghostping channel!\n_Use \`ghostping list\` to see all ghostping channels._`);
     }
-}
+  },
+};
