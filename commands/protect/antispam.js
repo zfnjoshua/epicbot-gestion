39769a9091fe65048b73ae25fb00f0b7e08e8e19
zfnjoshua <155145273+zfnjoshua@users.noreply.github.com>
2024-04-26 17:58:42 +0300

    improve

--- commands/protect/antispam.js
+++ commands/protect/antispam.js
@@ -1,46 +1,64 @@
-const db = require("quick.db")
+const db = require("quick.db");
 const Discord = require('discord.js');
-var rslow = require('../../slow.js');
+const rslow = require('../../slow.js');
 const { checkperm } = require("../../base/functions");
+
 module.exports = {
-        name: "antispam",
-        description: "Configure l'anti-spam",
-        usage: "antispam <off/on/max/allow/deny> <msg/time>",
-        aliases: ["anti-spam"],
+    name: "antispam",
+    description: "Configure the anti-spam",
+    usage: "antispam <off/on/max/allow/deny> <sensitivity>",
+    aliases: ["anti-spam"],
 
-        run: async (client, message, args, cmd) => {
-            let b = db.fetch(`${message.guild.id}.botowner`)
-            let perm = await checkperm(message, "anti")
-            if (perm == true || b && b.includes(message.author.id)) {
-            if (rslow.action[message.author.id] == true) return message.channel.send(`:x: Veuillez attendre avant d'effectuer une autre action !`)
-            let sensibilite = args[1]
-            let slash = 1
-            if(sensibilite) slash = sensibilite.indexOf("/")
-            if (args[0] === "on") {
-                if (args[1] && (isNaN(sensibilite.substring(0, slash)) || sensibilite[slash] !== "/" || isNaN(sensibilite.substring(slash+1, args[1].length)))) return message.reply(`:x: Veuillez préciser la sensibilité\nExemple: \`antispam <on/max> 6/5\``)
-                db.set(`${message.guild.id}.anti.spam`, "on")
-                if(sensibilite) db.set(`${message.guild.id}.anti.spam_sensi`, args[1] ? args[1] : "6/5")
-            } else
-            if (args[0] === "off") { db.delete(`${message.guild.id}.anti.spam`) } else
-            if (args[0] === "max") {
-                if (args[1] && (isNaN(sensibilite.substring(0, slash)) || sensibilite[slash] !== "/" || isNaN(sensibilite.substring(slash+1, args[1].length)))) return message.reply(`:x: Veuillez préciser la sensibilité\nExemple: \`antispam <on/max> 6/5\``)
-                db.set(`${message.guild.id}.anti.spam`, "max")
-                if(sensibilite) db.set(`${message.guild.id}.anti.spam_sensi`, args[1] ? args[1] : "6/5")
-            } else
-            if (args[0] === "allow") {
-                let channel = message.mentions.channels.first() || message.guild.channels.cache.get(args[1]) || message.channel
-                db.push(`${message.guild.id}.anti.spam_allow`, channel.id)
-                return message.reply(`:shield: Le salon \`${channel.name}\` est désormais ignoré par l'antispam`)
-            } else
-            if (args[0] === "deny") {
-                let channel = message.mentions.channels.first() || message.guild.channels.cache.get(args[1]) || message.channel
-                let channels = db.fetch(`${message.guild.id}.anti.spam_allow`)
-                const filtered = channels.filter(e => e !== channel.id);
-                db.set(`${message.guild.id}.anti.spam_allow`, filtered);
-                return message.reply(`:shield: Le salon \`${channel.name}\` n'est plus ignoré par l'antispam`)
-            } else return message.reply(`L'antispam est sur \`${db.fetch(`${message.guild.id}.anti.spam`) || "off"}\` !`)
+    run: async (client, message, args, cmd) => {
+        let b = db.fetch(`${message.guild.id}.botowner`)
+        let perm = await checkperm(message, "anti")
 
-            message.reply(`:shield: L'\`antispam\` est désormais sur \`${args[0]}\`${sensibilite ? " - \`"+sensibilite+"\`" : ""}${args[0] === "on" ? " (_les membres wl sont ignorés_)" : ""} `)
+        if (perm == true || b && b.includes(message.author.id)) {
+            if (rslow.action[message.author.id] == true) return message.channel.send(`:x: Please wait before performing another action!`)
+
+            const validActions = ["on", "off", "max", "allow", "deny"];
+            if (!args[0] || !validActions.includes(args[0])) return message.reply(`:x: Please specify a valid action. Valid actions are: \`${validActions.join("/")}\`!`)
+
+            let sensibilite = args[1];
+            let slashIndex = sensibilite ? sensibilite.indexOf("/") : -1;
+            if (args[0] === "on" || args[0] === "max") {
+                if (sensibilite && (slashIndex === -1 || isNaN(sensibilite.substring(0, slashIndex)) || sensibilite[slashIndex] !== "/" || isNaN(sensibilite.substring(slashIndex + 1, sensibilite.length)))) return message.reply(`:x: Please specify a valid sensitivity in the format \`number/number\`!`)
+            }
+
+            let newSetting;
+            switch (args[0]) {
+                case "on":
+                    newSetting = { spam: "on" };
+                    if (sensibilite) newSetting.spam_sensi = sensibilite;
+                    break;
+                case "off":
+                    newSetting = { spam: "off" };
+                    break;
+                case "max":
+                    newSetting = { spam: "max" };
+                    if (sensibilite) newSetting.spam_sensi = sensibilite;
+                    break;
+                case "allow":
+                    if (!args[1]) return message.reply(`:x: Please specify a channel to allow!`)
+                    let channel = message.mentions.channels.first() || message.guild.channels.cache.get(args[1]);
+                    if (!channel) return message.reply(`:x: Please specify a valid channel to allow!`)
+                    newSetting = { spam: "on", spam_allow: [...(db.fetch(`${message.guild.id}.anti.spam_allow`) || []), channel.id] };
+                    break;
+                case "deny":
+                    if (!args[1]) return message.reply(`:x: Please specify a channel to deny!`)
+                    let channel2 = message.mentions.channels.first() || message.guild.channels.cache.get(args[1]);
+                    if (!channel2) return message.reply(`:x: Please specify a valid channel to deny!`)
+                    let channels = db.fetch(`${message.guild.id}.anti.spam_allow`) || [];
+                    channels = channels.filter(e => e !== channel2.id);
+                    newSetting = { spam: "on", spam_allow: channels };
+                    break;
+            }
+
+            Object.keys(newSetting).forEach(key => {
+                db.set(`${message.guild.id}.anti.${key}`, newSetting[key]);
+            });
+
+            message.reply(`:shield: The \`antispam\` is now set to \`${args[0]}\`${sensibilite ? " - \`" + sensibilite + "\"": ""}${args[0] === "on" ? " (_ignoring bot owner messages_)" : ""} `)
 
             rslow.action[message.author.id] = true;
             setTimeout(() => {
@@ -51,9 +69,6 @@ module.exports = {
             if (logchannel) logchannel.send({
                 embeds: [new Discord.MessageEmbed()
                     .setColor(db.fetch(`${message.guild.id}.color`))
-                    .setDescription(`${message.author} a modifié l'antispam sur \`${args[0]}\` !`)]
+                    .setDescription(`${message.author} has changed the antispam to \`${args[0]}\`!`)]
             }).catch(e => { e })
-        } else if(perm === false) if(!db.fetch(`${message.guild.id}.vent`)) return message.reply(`:x: Vous n'avez pas la permission d'utiliser la commande \`anti\` !`)
-
-    }
-}
+        } else if (perm === false) if (!db.fetch(`${message.guild.id}.vent`)) return message.reply(`:x: You don't have permission to use the \
