afe0f6fe881587fc7ac90f5239bdb39eab28eedb
zfnjoshua <155145273+zfnjoshua@users.noreply.github.com>
2024-04-26 17:58:35 +0300

    improve

--- commands/utilitaire/avatar.js
+++ commands/utilitaire/avatar.js
@@ -1,27 +1,43 @@
+const { Command } = require('discord.js-commando');
+const { checkPerms } = require('../../utils/functions');
 
-const db = require("quick.db")
-const Discord = require('discord.js');
-const { checkperm } = require("../../base/functions");
-module.exports = {
-    name: "avatar",
-    description: "Affiche l'image de profil du membre mentionné",
-    aliases: ["pp", "pic"],
+module.exports = class AvatarCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'avatar',
+      group: 'misc',
+      memberName: 'avatar',
+      description: 'Displays the avatar of the mentioned user.',
+      aliases: ['pp', 'pic'],
+      throttling: {
+        usages: 2,
+        duration: 3
+      },
+      args: [
+        {
+          key: 'member',
+          prompt: 'Please mention a user or provide a user ID.',
+          type: 'member'
+        }
+      ]
+    });
+  }
 
-    run: async (client, message, args, cmd) => {
-        let perm = await checkperm(message,cmd.name)
-        if (perm == true) {
-            let member = await message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(r => r.user.tag.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.guild.members.cache.find(r => r.displayName.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.member
-            if (!member || member.bot) return message.channel.send(`:x: Utilisateur invalide !`)
-            let avatarembed = new Discord.MessageEmbed()
-            .setImage(member.displayAvatarURL({dynamic: true, size: 512}))
-            .setColor(db.fetch(`${message.guild.id}.color`))
-            .setTitle("Avatar de "+member.user.username)
-            .setFooter({text: "Demandé par " + message.author.tag})
-            .setDescription("[Lien de l'avatar]("+ member.displayAvatarURL({dynamic: true, size: 512}) +")")
-        
+  run(message, { member }) {
+    const perm = checkPerms(message.member, this.name);
 
-            message.reply({embeds: [avatarembed]})
-        } else if(perm === false) if(!db.fetch(`${message.guild.id}.vent`)) return message.reply(`:x: Vous n'avez pas la permission d'utiliser la commande \`${cmd.name}\` !`)
-
+    if (!perm.allowed) {
+      if (!perm.silent) message.reply(perm.msg);
+      return;
     }
-}
+
+    const avatarEmbed = new Discord.MessageEmbed()
+      .setImage(member.user.displayAvatarURL({ dynamic: true, size: 512 }))
+      .setColor(message.guild.me.displayHexColor)
+      .setTitle(`Avatar of ${member.user.username}`)
+      .setFooter(`Requested by ${message.author.tag}`)
+      .setDescription(`[Avatar Link](${member.user.displayAvatarURL({ dynamic: true, size: 512 })})`);
+
+    message.embed(avatarEmbed);
+  }
+};
