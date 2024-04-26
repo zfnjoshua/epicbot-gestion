78dce5b9b5f5f89cca492e2af57c50bbe5e4b54a
zfnjoshua <155145273+zfnjoshua@users.noreply.github.com>
2024-04-26 17:58:38 +0300

    improve

--- commands/protect/secur.js
+++ commands/protect/secur.js
@@ -13,33 +13,47 @@ module.exports = {
 
             const secur = client.config.secur
             let logchannel = db.fetch(`${message.guild.id}.raidlogs`)
-            logchannel = message.guild.channels.cache.get(logchannel)
+            if (logchannel) logchannel = message.guild.channels.cache.get(logchannel)
             let adminlogs = db.fetch(`${message.guild.id}.adminlogs`)
-            adminlogs = message.guild.channels.cache.get(adminlogs)
+            if (adminlogs) adminlogs = message.guild.channels.cache.get(adminlogs)
             let serverlock = db.fetch(`${message.guild.id}.serverlock`)
+            if (!serverlock) serverlock = false
             if (!args[0]) {
                 const Embed = new Discord.MessageEmbed()
                     .setColor(db.fetch(`${message.guild.id}.color`))
                     .setTitle(`Secur`)
                 let array = []
-                for (i in secur) {
-                    let p = db.fetch(`${message.guild.id}.anti.${secur[i]}`)
-                    let sensi = db.fetch(`${message.guild.id}.anti.${secur[i]}_sensi`)
-                    array.push(`**Anti${secur[i]}:** ` + (p ? "``" + p + "``" : "``off``") + (sensi ? " - ``" + sensi + "``" : ""))
+                if (db.fetch(`${message.guild.id}.anti`)) {
+                    for (i in secur) {
+                        let p = db.fetch(`${message.guild.id}.anti.${secur[i]}`)
+                        if (p) p = `\`\`${p}\`\``
+                        else p = '`off`'
+                        let sensi = db.fetch(`${message.guild.id}.anti.${secur[i]}_sensi`)
+                        if (sensi) sensi = ` - \`${sensi}\``
+                        else sensi = ''
+                        array.push(`**Anti${secur[i]}:** ${p}${sensi}`)
+                    }
                 }
                 let lockedurl = db.fetch(`${message.guild.id}.lockedurl`)
-Embed.setDescription(`**Logs de raid**: ${logchannel ? logchannel : ":x:"}
-**Logs d'alerte**: ${adminlogs ? adminlogs : ":x:"}
-${serverlock ? ":warning: " : ""}**Lock Server**: ${serverlock ? serverlock : ":x:"}
-**Lock URL**: ${lockedurl && message.guild.vanityURLCode ? `\`on\` - \`${lockedurl}\`` : "`off`"}
-` + array.map(object => object).join("\n"))
+                Embed.setDescription(`**Logs de raid**: ${logchannel ? logchannel : ":x:"}
+                **Logs d'alerte**: ${adminlogs ? adminlogs : ":x:"}
+                ${serverlock ? ":warning: " : ""}**Lock Server**: ${serverlock ? serverlock : ":x:"}
+                **Lock URL**: ${lockedurl && message.guild.vanityURLCode ? `\`on\` - \`${lockedurl}\`` : "`off`"}
+                ` + (array.length ? array.join("\n") : ""))
                 message.reply({ embeds: [Embed] })
             } else {
                 const parameters = ["on", "max", "off"]
                 let setup = args[0].toLowerCase()
                 if (parameters.includes(setup)) {
-                    for (i in secur) {
-                        db.set(`${message.guild.id}.anti.${secur[i]}`, setup)
+                    if (db.fetch(`${message.guild.id}.anti`)) {
+                        for (i in secur) {
+                            db.set(`${message.guild.id}.anti.${secur[i]}`, setup)
+                        }
+                    } else {
+                        db.set(`${message.guild.id}.anti`, {})
+                        for (i in secur) {
+                            db.set(`${message.guild.id}.anti.${secur[i]}`, setup)
+                        }
                     }
                     message.reply(`:shield: Toutes les sécurités ont été mises sur \`\`${setup}\`\` !`)
                 }
