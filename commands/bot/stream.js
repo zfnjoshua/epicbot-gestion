5c05de321717a987d332f2b222de096c22afe963
zfnjoshua <155145273+zfnjoshua@users.noreply.github.com>
2024-04-26 17:58:32 +0300

    improve

--- commands/bot/stream.js
+++ commands/bot/stream.js
@@ -1,19 +1,50 @@
 const { checkperm } = require("../../base/functions");
 const db = require('quick.db')
+
 module.exports = {
     name: "stream",
     description: "Modifie le statut en _stream_",
 
     run: async (client, message, args, cmd) => {
-        let perm = await checkperm(message, "activity")
-        if (perm == true) {
-            let content = args.join(" ")
-            if(!content) return message.reply("Veuillez préciser le contenu du statut !")
-            db.set(`bottype`, "STREAMING")
-            db.set(`botactivity`, content)
-            client.user.setActivity(content, { type: "STREAMING", url: "https://www.twitch.tv/coinsbot" })
-            message.reply(`Je stream maintenant \`${content}\` !`)
-        } else if(perm === false) if(!db.fetch(`${message.guild.id}.vent`)) return message.reply(`:x: Vous n'avez pas la permission d'utiliser la commande \`activity\` !`)
+        try {
+            let perm = await checkperm(message, "activity")
+            if (perm === false) {
+                if (!db.fetch(`${message.guild.id}.vent`)) return message.channel.send(`:x: Vous n'avez pas la permission d'utiliser la commande \`activity\` !`);
+            } else if (perm == true) {
+                if (args.length < 1) return message.channel.send("Veuillez préciser le contenu du statut !");
+                let content = args.join(" ");
+                if (!content) return message.channel.send("Veuillez préciser le contenu du statut !");
+                try {
+                    db.set(`bottype`, "STREAMING");
+                    db.set(`botactivity`, content);
+                } catch (err) {
+                    console.error(err);
+                    return message.channel.send("Une erreur est survenue lors de la modification du statut !");
+                }
+                try {
+                    if (client.user && client.user.presence && client.user.presence.activities && client.user.presence.activities[0]) {
+                        client.user.presence.activities[0].type = "STREAMING";
+                        client.user.presence.activities[0].name = content;
+                        client.user.presence.activities[0].url = "https://www.twitch.tv/coinsbot";
+                    } else {
+                        client.user.setPresence({
+                            status: 'online',
+                            activities: [{
+                                name: content,
+                                type: 'STREAMING',
+                                url: 'https://www.twitch.tv/coinsbot'
+                            }]
+                        });
+                    }
+                } catch (err) {
+                    console.error(err);
+                    return message.channel.send("Une erreur est survenue lors de la modification du statut !");
+                }
+                message.channel.send(`Je stream maintenant \`${content}\` !`);
+            }
+        } catch (err) {
+            console.error(err);
+            message.channel.send("Une erreur est survenue lors de l'exécution de la commande !");
+        }
     }
-
 }
