29b74b0924b533ddb454bfe9e9423b04693dc65d
zfnjoshua <155145273+zfnjoshua@users.noreply.github.com>
2024-04-26 17:58:27 +0300

    improve

--- commands/logs/adminlogs.js
+++ commands/logs/adminlogs.js
@@ -1,25 +1,33 @@
 const db = require("quick.db")
 const { checkperm } = require("../../base/functions");
+
 module.exports = {
   name: "adminlogs",
-  description: "Défini les logs d'alerte lorsque qu'une permission admin est ajoutée",
+  description: "Defines the logs alert when an admin permission is added",
   aliases: ['adminlog'],
 
   run: async (client, message, args, cmd) => {
     let perm = await checkperm(message, "logs")
-    if (perm == true) {
 
-      let m = message.mentions.channels.first() || message.guild.channels.cache.get(args[0]) || message.channel
-      if (!m || m.type !== "GUILD_TEXT") return message.reply(`:x: Salon invalide !`)
-      let actual = db.fetch(`${message.guild.id}.adminlogs`)
-      if (actual === m.id) {
-        db.delete(`${message.guild.id}.adminlogs`)
-        return message.reply(`:clipboard: Les logs de permission administrateur ajoutée sont désactivés !`)
-      }
-      db.set(`${message.guild.id}.adminlogs`, m.id)
-      return message.reply(`:clipboard: Les logs de permission administrateur ajoutée seront maintenant envoyés dans <#${m.id}> !`)
+    if (!perm) {
+      if (db.fetch(`${message.guild.id}.vent`)) return;
+      return message.reply(`:x: You don't have permission to use the \`logs\` command!`);
+    }
 
+    let m = message.mentions.channels.first() || message.guild.channels.cache.get(args[0]) || message.channel;
 
-    } else if(perm === false) if(!db.fetch(`${message.guild.id}.vent`)) return message.reply(`:x: Vous n'avez pas la permission d'utiliser la commande \`logs\` !`)
+    if (!m || m.type !== "GUILD_TEXT") {
+      return message.reply(`:x: Invalid channel mentioned or ID provided!`);
+    }
+
+    let actual = await db.fetch(`${message.guild.id}.adminlogs`);
+
+    if (actual === m.id) {
+      await db.delete(`${message.guild.id}.adminlogs`);
+      return message.reply(`:clipboard: Admin permission logs are now disabled!`);
+    }
+
+    await db.set(`${message.guild.id}.adminlogs`, m.id);
+    return message.reply(`:clipboard: Admin permission logs will now be sent in <#${m.id}>!`);
   }
-}
+};
