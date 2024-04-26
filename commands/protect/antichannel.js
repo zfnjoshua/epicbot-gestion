352d53449f8afcf7b67ef6ca7ce05c10a0142aaf
zfnjoshua <155145273+zfnjoshua@users.noreply.github.com>
2024-04-26 17:58:33 +0300

    improve

--- commands/protect/antichannel.js
+++ commands/protect/antichannel.js
@@ -1,36 +1,54 @@
-const db = require("quick.db")
+const db = require("quick.db");
 const Discord = require('discord.js');
-var rslow = require('../../slow.js');
+const rslow = require('../../slow.js');
 const { checkperm } = require("../../base/functions");
+
 module.exports = {
-        name: "antichannel",
-        description: "Configure l'anti-channel",
-        usage: "antichannel <off/on/max>",
-        aliases: ["anti-channel"],
+    name: "antichannel",
+    description: "Configure the anti-channel",
+    usage: "antichannel <off/on/max>",
+    aliases: ["anti-channel"],
 
-        run: async (client, message, args, cmd) => {
-            let b = db.fetch(`${message.guild.id}.botowner`)
-            let perm = await checkperm(message, "anti")
-            if (perm == true || b && b.includes(message.author.id)) {
-            if (rslow.action[message.author.id] == true) return message.channel.send(`:x: Veuillez attendre avant d'effectuer une autre action !`)
-            if (args[0] === "on") { db.set(`${message.guild.id}.anti.channel`, "on") } else
-                if (args[0] === "off") { db.delete(`${message.guild.id}.anti.channel`) } else
-                    if (args[0] === "max") { db.set(`${message.guild.id}.anti.channel`, "max") } else return message.reply(`L'antichannel est sur \`${db.fetch(`${message.guild.id}.anti.channel`) || "off"}\` !`)
+    run: async (client, message, args, cmd) => {
+        const botOwner = db.fetch(`${message.guild.id}.botowner`);
+        const perm = await checkperm(message, "anti");
 
-            message.reply(`:shield: L'\`antichannel\` est désormais sur \`${args[0]}\`${args[0] === "on" ? " (_les membres wl sont ignorés_)" : ""}`)
+        if (!(perm || botOwner && botOwner.includes(message.author.id))) {
+            if (!db.fetch(`${message.guild.id}.vent`)) {
+                return message.reply(`:x: You don't have permission to use the \`anti\` command!`);
+            }
+        }
 
-            rslow.action[message.author.id] = true;
-            setTimeout(() => {
-                rslow.action[message.author.id] = false;
-            }, db.fetch(`${message.guild.id}.actionslow`));
-            let logchannel = db.fetch(`${message.guild.id}.raidlogs`)
-            logchannel = message.guild.channels.cache.get(logchannel)
-            if (logchannel) logchannel.send({
+        if (rslow.action[message.author.id]) {
+            return message.channel.send(`:x: Please wait before performing another action!`);
+        }
+
+        const validOptions = ["on", "off", "max"];
+        if (!validOptions.includes(args[0])) {
+            return message.reply(`L'antichannel est sur \`${db.fetch(`${message.guild.id}.anti.channel`) || "off"}\` !`);
+        }
+
+        db.set(`${message.guild.id}.anti.channel`, args[0]);
+
+        message.reply(`:shield: L'\`antichannel\` est désormais sur \`${args[0]}\`${args[0] === "on" ? " (_les membres wl sont ignorés_)" : ""}`);
+
+        rslow.action[message.author.id] = true;
+        setTimeout(() => {
+            rslow.action[message.author.id] = false;
+        }, db.fetch(`${message.guild.id}.actionslow`));
+
+        const logChannel = db.fetch(`${message.guild.id}.raidlogs`);
+        const logChannelObj = message.guild.channels.cache.get(logChannel);
+
+        if (logChannelObj) {
+            logChannelObj.send({
                 embeds: [new Discord.MessageEmbed()
                     .setColor(db.fetch(`${message.guild.id}.color`))
-                    .setDescription(`${message.author} a modifié l'antichannel sur \`${args[0]}\` !`)]
-            }).catch(e => { e })
-        } else if(perm === false) if(!db.fetch(`${message.guild.id}.vent`)) return message.reply(`:x: Vous n'avez pas la permission d'utiliser la commande \`anti\` !`)
-
+                    .setDescription(`${message.author} has modified the antichannel to \`${args[0]}\`!`)
+                ]
+            }).catch(e => {
+                console.error(e);
+            });
+        }
     }
-}
+};
