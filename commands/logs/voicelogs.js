6a649e074b1ad6e22db4ec2cbfa7a48f4f5bd37d
zfnjoshua <155145273+zfnjoshua@users.noreply.github.com>
2024-04-26 17:58:27 +0300

    improve

--- commands/logs/voicelogs.js
+++ commands/logs/voicelogs.js
@@ -1,25 +1,34 @@
-const db = require("quick.db")
+const db = require("quick.db");
 const { checkperm } = require("../../base/functions");
+
 module.exports = {
     name: "voicelogs",
     description: "Défini les logs vocal",
     aliases: ['voicelog', "voclog"],
 
     run: async (client, message, args, cmd) => {
-        let perm = await checkperm(message, "logs")
-        if (perm == true) {
+        let perm = await checkperm(message, "logs");
 
-            let m = message.mentions.channels.first() || message.guild.channels.cache.get(args[0]) || message.channel
-            if (!m || m.type !== "GUILD_TEXT") return message.reply(`:x: Salon invalide !`)
-            let actual = db.fetch(`${message.guild.id}.voicelogs`)
-            if (actual === m.id) {
-                db.delete(`${message.guild.id}.voicelogs`)
-                return message.reply(`:clipboard: Les logs vocal sont désactivés !`)
+        if (!perm) {
+            if (!db.fetch(`${message.guild.id}.vent`)) {
+                return message.reply(`:x: Vous n'avez pas la permission d'utiliser la commande \`logs\` !`);
             }
-            db.set(`${message.guild.id}.voicelogs`, m.id)
-            return message.reply(`:clipboard: Les logs vocal seront maintenant envoyés dans <#${m.id}> !`)
+        }
 
+        let m = message.mentions.channels.first() || message.guild.channels.cache.get(args[0]);
 
-        } else if(perm === false) if(!db.fetch(`${message.guild.id}.vent`)) return message.reply(`:x: Vous n'avez pas la permission d'utiliser la commande \`logs\` !`)
+        if (!m || m.type !== "GUILD_TEXT") {
+            return message.reply(`:x: Veuillez mentionner un salon texte valide !`);
+        }
+
+        let actual = await db.fetch(`${message.guild.id}.voicelogs`);
+
+        if (actual === m.id) {
+            await db.delete(`${message.guild.id}.voicelogs`);
+            return message.reply(`:clipboard: Les logs vocal sont désactivés !`);
+        }
+
+        await db.set(`${message.guild.id}.voicelogs`, m.id);
+        return message.reply(`:clipboard: Les logs vocal seront maintenant envoyés dans <#${m.id}> !`);
     }
-}
+};
