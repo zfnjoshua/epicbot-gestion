fe0e21e014650b6e923ef2564f658bfd4c578d63
zfnjoshua <155145273+zfnjoshua@users.noreply.github.com>
2024-04-26 17:58:39 +0300

    improve

--- commands/utilitaire/snipe.js
+++ commands/utilitaire/snipe.js
@@ -1,30 +1,52 @@
-const db = require("quick.db")
-const Discord = require('discord.js');
-const { checkperm } = require("../../base/functions");
-module.exports = {
-    name: "snipe",
-    description: "Affiche le dernier message supprimé",
+const { Command } = require('discord.js-commando');
+const db = require('quick.db');
+const fetch = require('node-fetch');
 
-    run: async (client, message, args, cmd) => {
-        let perm = await checkperm(message, cmd.name)
-        if (perm == true) {
-            const msg = client.snipes.get(message.channel.id)
-            if (!msg) return message.channel.send(":x: Il n'y a pas de message à snipe !")
-            let slash
-            let content = msg.content
-            if (msg.content.includes("gg/")) {
-                slash = content.indexOf("/")
-                content = msg.content.replace(msg.content.substring(slash + 1, msg.content.length), "••••••••")
-            }
-            const embed = new Discord.MessageEmbed()
-                .setAuthor({ name: msg.author, iconURL: msg.profilephoto })
-                .setDescription(content)
-                .setColor(db.fetch(`${message.guild.id}.color`))
-                .setTimestamp(msg.date)
-            if (msg.image) embed.setImage(msg.image)
+module.exports = class SnipeCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'snipe',
+      group: 'mod',
+      memberName: 'snipe',
+      description: 'Displays the last deleted message',
+      throttling: {
+        usages: 2,
+        duration: 3
+      }
+    });
+  }
 
-            message.reply({ embeds: [embed] })
-        } else if(perm === false) if(!db.fetch(`${message.guild.id}.vent`)) return message.reply(`:x: Vous n'avez pas la permission d'utiliser la commande \`${cmd.name}\` !`)
+  async run(message) {
+    const snipeData = this.client.snipes.get(message.channel.id);
 
+    if (!snipeData) {
+      return message.reply(':x: There is no message to snipe!');
     }
-}
+
+    let content = snipeData.content;
+    if (content.includes('gg/')) {
+      const slashIndex = content.indexOf('/');
+      content = content.replace(content.substring(slashIndex + 1), '••••••••');
+    }
+
+    const embed = new MessageEmbed()
+      .setAuthor({ name: snipeData.author.username, iconURL: snipeData.author.displayAvatarURL() })
+      .setDescription(content)
+      .setColor(db.fetch(`${message.guild.id}.color`))
+      .setTimestamp(snipeData.date);
+
+    if (snipeData.image) embed.setImage(snipeData.image);
+
+    return message.reply({ embeds: [embed] });
+  }
+
+  checkPerm(message, cmd) {
+    if (!message.member.permissions.has('MANAGE_MESSAGES')) {
+      if (!db.fetch(`${message.guild.id}.vent`)) {
+        return message.reply(`:x: You don't have permission to use the command \`${cmd.name}\`!`);
+      }
+    }
+
+    return true;
+  }
+};
